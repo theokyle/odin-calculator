d905ed673c9075d10b67017145f5f029b31a0409
theokyle <121832097+theokyle@users.noreply.github.com>
2024-09-07 00:43:19 -0500

    Equals now calculates a simple string

--- javascript.js
+++ javascript.js
@@ -16,14 +16,25 @@ function divide(num1, num2) {
 };
 
 const numberButtons = document.querySelectorAll(".number");
-const operatorBUttons = document.querySelectorAll(".operator");
+const operatorButtons = document.querySelectorAll(".operator");
 const display = document.querySelector(".display");
+const equals = document.querySelector(".equals");
 let displayText = "";
 
 numberButtons.forEach((number) => number.addEventListener("click", () => {
     displayText += number.id;
     display.textContent = displayText;
 } ));
+
+operatorButtons.forEach((operator) => operator.addEventListener("click", () => {
+    displayText += " " + operator.id + " ";
+    display.textContent = displayText;
+} ));
+
+equals.addEventListener("click", () => {
+    let equation = displayText.split(" ");
+    operate(Number(equation[0]),Number(equation[2]), equation[1]);
+});
 
 function operate(num1, num2, operator) {
     let result;
