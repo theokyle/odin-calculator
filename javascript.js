77ba66ecdc98a8cf07d8c14c8f5a02645c749f5c
theokyle <121832097+theokyle@users.noreply.github.com>
2024-09-07 01:44:12 -0500

    All buttons now functional

--- javascript.js
+++ javascript.js
@@ -19,6 +19,8 @@ const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator");
 const display = document.querySelector(".display");
 const equals = document.querySelector(".equals");
+const clear = document.querySelector("#clear");
+const backspace = document.querySelector("#backspace");
 let displayText = "";
 
 numberButtons.forEach((number) => number.addEventListener("click", () => {
@@ -27,6 +29,8 @@ numberButtons.forEach((number) => number.addEventListener("click", () => {
 } ));
 
 operatorButtons.forEach((operator) => operator.addEventListener("click", () => {
+    let equation = displayText.split(" ");
+    operate(Number(equation[0]),Number(equation[2]), equation[1]);
     displayText += " " + operator.id + " ";
     display.textContent = displayText;
 } ));
@@ -36,12 +40,33 @@ equals.addEventListener("click", () => {
     operate(Number(equation[0]),Number(equation[2]), equation[1]);
 });
 
+clear.addEventListener("click", () => {
+    displayText = "";
+    display.textContent = displayText;
+});
+
+backspace.addEventListener("click", () => {
+    let newText = displayText.split("");
+    if(newText.pop() == " ") {
+        newText.pop();
+        newText.pop();
+    }
+    displayText = newText.join("");
+    display.textContent = displayText;
+});
+
 function operate(num1, num2, operator) {
     let result;
     switch (operator) {
         case "/":
+            if (num2 === 0){
+                alert("Oops. You can't divide by 0!");
+                displayText = "";
+                display.textContent = displayText;
+                break;
+            }
             result = divide(num1,num2);
-            displayText = result.toString;
+            displayText = result.toString();
             display.textContent = displayText;
             break;
         case "*":
